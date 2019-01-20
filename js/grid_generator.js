cc2eb9f2488026e9b75ab9bb1989670323e8257f
theo <theo.quiquempoix@polymtl.ca>
2019-01-19 21:40:04 -0500

    Addition de la premiere map geojson genere par algo

--- js/grid_generator.js
+++ js/grid_generator.js
@@ -1,8 +1,15 @@
 var obj = {
-    table: []
- };
- 
-const zoneSize = 100;
+    type: "Featureblal",
+    crs: {
+        type: "name",
+        properties: {
+            name: "x:y"
+        }
+    },
+    features: []
+};
+
+const zoneSize = 10;
 const squareSize = 0.0013;
 const xStart = 45.4800;
 const yStart = -73.6800;
@@ -10,15 +17,40 @@ const yStart = -73.6800;
 let x = xStart;
 let y = yStart;
 
-const fs = require('fs');
+let xPlus1 = xStart;
+let yPlus1 = yStart;
 
- for(i = 0; i < zoneSize; i++) {
-     x = xStart;
-    for(j=0;j<zoneSize; j++) {
+for (i = 0; i < zoneSize; i++) {
+    x = xStart;
+    for (j = 0; j < zoneSize; j++) {
+        xPlus1 = x + squareSize;
+        yPlus1 = y + squareSize;
+
+        obj.features.push({
+            type: "Feature",
+            properties: {
+                coordinates: i+":"+j
+            },
+            geometry: {
+                type: "Polygon",
+                coordinates: [
+                    [
+                        [x,y],
+                        [xPlus1,y],
+                        [xPlus1,yPlus1],
+                        [x,yPlus1],
+                        [x,y]
+                    ]
+                ]
+            }
+        });
         x += squareSize;
-        obj.table.push({id1: i, id2: j, coordX: x, coordY: y});
-        var json = JSON.stringify(obj);
-        fs.writeFile('squares_map.json', json, 'utf8', callback);
-     }
-     y += squareSize;
- }
+    }
+    y += squareSize;
+}
+var json = JSON.stringify(obj);
+localStorage.setItem("Coord", json);
+
+console.log(localStorage.getItem('Coord'));
+
+//let coord = localStorage.getItem('Coord');
