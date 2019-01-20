0c409bd29b948c499cfed065229559a2b1ae556d
Alexandre Talens <alexandre.talens@polymtl.ca>
2019-01-19 22:29:17 -0500

    parcours de la grille

--- js/score.js
+++ js/score.js
@@ -1,5 +1,3 @@
-// Téléchargement de la grille de la ville
-var grid = "http://donnees.ville.montreal.qc.ca/dataset/e160983c-f5d2-49ec-9b81-d0f6fb66518a/resource/d05e6306-9537-40cb-b995-1d8830febe19/download/sqrc_500.geojson";
 
 // Definition d'un objet type de donneées
 /*
@@ -23,5 +21,10 @@ $.getJSON( "https://storage.googleapis.com/confortappart/data_list3.json", funct
   console.log(dataFiles[0]);
 });
 
+// On recupere la grille generee
+let grid = localStorage.getItem("Coord");
 
 
+for (i=0; i < grid.features.length; i++) {
+    grid.features[i].geometry.coordinates[0][0]
+};
